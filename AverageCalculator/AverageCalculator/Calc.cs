c52399c2457b74de4282046dd7675a708b1fa04e
pedro-n-garcez <60759070+pedro-n-garcez@users.noreply.github.com>
2020-05-23 18:32:48 -0300

    Add files via upload

---
+++ AverageCalculator/AverageCalculator/Calc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AverageCalculator
+{
+    class Calc
+    {
+        public List<double> items = new List<double>();
+        double average;
+
+        public Calc()
+        {
+        }
+
+        public double Calculate() => average = items.Sum() / items.Count;
+
+        public string allNumbers()
+        {
+            string s = String.Empty;
+            foreach (double d in items)
+            {
+                s += d + "\n";
+            } return s;
+        }
+    }
+}
---
+++ AverageCalculator/AverageCalculator/MainPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AverageCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        Calc c;
+        string input_text;
+        double input;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            c = new Calc();
+        }
+
+        private void textBox_numberInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            input_text = textBox_numberInput.Text;
+        }
+
+        private void button_Add_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                input = Double.Parse(input_text);
+                c.items.Add(input);
+                textBlock_numbersAdded.Text = c.allNumbers();
+                textBlock_average.Text = c.Calculate().ToString();
+            } catch (ArgumentNullException)
+            {
+                textBox_numberInput.Text = "Invalid input.";
+            } catch (FormatException)
+            {
+                textBox_numberInput.Text = "Invalid input.";
+            }
+        }
+
+        private void button_Reset_Click(object sender, RoutedEventArgs e)
+        {
+            c.items.Clear();
+            textBlock_numbersAdded.Text = c.allNumbers();
+            if (c.items.Count() != 0)
+                textBlock_average.Text = c.Calculate().ToString();
+            else
+                textBlock_average.Text = String.Empty;
+        }
+
+        private void button_RemoveLast_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                c.items.RemoveAt(c.items.Count() - 1);
+                textBlock_numbersAdded.Text = c.allNumbers();
+                if (c.items.Count() != 0)
+                    textBlock_average.Text = c.Calculate().ToString();
+                else
+                    textBlock_average.Text = String.Empty;
+            } catch (ArgumentOutOfRangeException)
+            {
+                textBlock_numbersAdded.Text = "You have no numbers.";
+            }
+        }
+    }
+}
